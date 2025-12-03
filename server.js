92ec324b4e83675ae8a06703825db5b9d257088a
DanielHelmrosi <daniel.helmrosin@tptlive.ee>
2025-12-03 13:56:35 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+﻿const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.post("/api/location", (req, res) => {
+    const { lat, lon, accuracy } = req.body;
+
+    console.log("Получена геолокация:");
+    console.log("Широта:", lat);
+    console.log("Долгота:", lon);
+    console.log("Точность:", accuracy + "м");
+
+    const log = `LAT: ${lat}, LON: ${lon}, ACC: ${accuracy}, TIME: ${new Date().toISOString()}\n`;
+    fs.appendFileSync("locations.log", log);
+
+    res.json({ status: "OK" });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log("Сервер запущен: http://localhost:" + PORT));
